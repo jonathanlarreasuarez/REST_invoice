b146841d0fe6e89a530aa74d85e641742fb964b6
Jonathan Larrea Suárez <jonathanlarreasuarez@gmail.com>
2021-11-22 00:00:35 -0500

    Creación de modelos y migración

--- REST_invoice/settings.py
+++ REST_invoice/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',  # rest Framework
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
--- api/models.py
+++ api/models.py
@@ -1,3 +1,57 @@
+"""Posts models."""
+
+# Django
 from django.db import models
 
-# Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Sku(models.Model):
+    reference = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
+    price = models.FloatField()
+    brand = models.ForeignKey(
+        Brand, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE)
+
+
+class Item(models.Model):
+    gross = models.FloatField()
+    discounts = models.FloatField()
+    subtotal = models.FloatField()
+    tax = models.FloatField()
+    total = models.FloatField()
+    # sku = models.ManyToManyField(Sku)
+    sku = models.ForeignKey(
+        Sku, on_delete=models.CASCADE)
+
+
+class Customer(models.Model):
+    document_type = models.CharField(max_length=25)
+    document_number = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=25)
+    address = models.CharField(max_length=150)
+    email = models.CharField(max_length=100)
+
+
+class Invoice(models.Model):
+    createtime = models.DateTimeField()
+    document_number = models.CharField(max_length=16)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE)
+    # item = models.ForeignKey(
+    #     Item, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Item)
+
+    def __str__(self):
+        return self.document_number
+
